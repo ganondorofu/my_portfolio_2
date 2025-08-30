d4e010a22100e434865fb6694157953fe54fe12d
ganondorofu <ganondorofu123@gmail.com>
2025-08-30 02:45:52 +0000

    GET https://komeniki.net/your-repo-name/_next/static/css/48937521a3627c1

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type {NextConfig} from 'next';
 const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
 
 // Replace `your-repo-name` with the name of your repository
-const repoName = 'your-repo-name';
+const repoName = 'yoneyone-portfolio';
 
 const nextConfig: NextConfig = {
   output: 'export',
