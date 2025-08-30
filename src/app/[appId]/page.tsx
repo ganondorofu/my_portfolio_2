a7c19cd0a10557dcb46fe7c9524c4c8964ca79f9
ganondorofu <ganondorofu123@gmail.com>
2025-08-30 11:03:09 +0000

    src/app/[appId]/page.tsxにsitemap.xmlを作るためのやつを作ってる もう少し簡単に書けるようにして

--- src/app/[appId]/page.tsx
+++ src/app/[appId]/page.tsx
@@ -3,6 +3,15 @@
 import { useAppManager } from '@/hooks/useAppManager';
 import AppWindow from '@/components/portfolio/AppWindow';
 import FishEasterEgg from '@/components/portfolio/FishEasterEgg';
+import { apps } from '@/lib/apps';
+
+export async function generateStaticParams() {
+  return Object.values(apps)
+    .filter(app => !app.externalUrl && app.content)
+    .map(app => ({
+      appId: app.id,
+    }));
+}
 
 export default function AppView() {
   const { activeApp, apps } = useAppManager();
