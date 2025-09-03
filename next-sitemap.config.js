ddc95141b21f89e63de921e8cbc9efbc128ef9cd
ganondorofu <ganondorofu123@gmail.com>
2025-09-03 15:56:52 +0000

    seoいい？

--- next-sitemap.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: "https://komeniki.net",
-    autoLastmod: false,
-    generateIndexSitemap: false,
-    generateRobotsTxt: false,
-    outDir: "./public",
-};
